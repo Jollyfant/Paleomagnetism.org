f6ef67e626e7e97fecc0ddf2a36731cdb840b804
Jollyfant <m.r.koymans@students.uu.nl>
2016-09-12 20:34:01 +0200

    Added Tibet/Sangri pmag results; renamed verified to published

--- Paleomagnetism.org/src/db/pubs.js
+++ Paleomagnetism.org/src/db/pubs.js
@@ -1,4 +1,6 @@
 var PUBLICATIONS = {
   "c440d9d38fe0ee981c8b8719c026c55d": "Vissers et al., 2016: Cretaceous slab break-off in the Pyrenees: Iberian plate kinematics in paleomagnetic and mantle reference frames",
-  "1a79a4d60de6718e8e5b326e338ae533": "Example Dir"
+  "1a79a4d60de6718e8e5b326e338ae533": "Example Dir",
+  "c64b185ccb115f30d452c89008cb6bb0": "Tibet, Li, Z. et al.,  2016, Paleomagnetic constraints on the Mesozoic drift of the Lhasa terrane (Tibet) from Gondwana to Eurasia, Geology 44, p. 727-730",
+  "3dddfe075333c3d451a813a5cfad1b31": "Sangri, Li, Z. et al.,  2016, Paleomagnetic constraints on the Mesozoic drift of the Lhasa terrane (Tibet) from Gondwana to Eurasia, Geology 44, p. 727-730"
  }
